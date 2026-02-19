9356540b89d73475a77f4df07dd177413a5d4fdd
ss0801432 <1124207153@qq.com>
2026-02-19 16:23:54 +0800

    更新日志记录时机

--- src/utils/get_logger.py
+++ src/utils/get_logger.py
@@ -1,40 +1,35 @@
-from datetime import datetime
 import logging
-import uuid
 import os
+from logging.handlers import TimedRotatingFileHandler
 
 from src.utils.get_root_path import get_project_root
 
 
-def get_logger(name="deepagent_stream", thread_id=None):
-    """
-    获取logger
-    """
+def get_logger(name="deepagent_stream"):
+    logger = logging.getLogger(name)
 
-    if thread_id is None:
-        thread_id = f"{uuid.uuid4().hex[:8]}"
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
 
     project_root = get_project_root()
     log_dir = os.path.join(project_root, "logs")
     os.makedirs(log_dir, exist_ok=True)
 
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
+    log_path = os.path.join(log_dir, f"{name}.log")
 
-    # 防止重复添加 handler
-    if not logger.handlers:
-        log_path = os.path.join(
-            log_dir,
-            f"{name}_{datetime.now().strftime('%Y-%m-%d—%H-%M-%S')}_{thread_id}.log"
-        )
+    handler = TimedRotatingFileHandler(
+        log_path,
+        when="midnight",
+        interval=1,
+        backupCount=30,
+        encoding="utf-8",
+    )
+    handler.suffix = "%Y-%m-%d"
+    handler.setFormatter(logging.Formatter("%(asctime)s | %(levelname)s | %(message)s"))
 
-        handler = logging.FileHandler(log_path, encoding="utf-8")
-        formatter = logging.Formatter(
-            "%(asctime)s | %(levelname)s | %(message)s"
-        )
-        handler.setFormatter(formatter)
-
-        logger.addHandler(handler)
+    logger.addHandler(handler)
 
     return logger
